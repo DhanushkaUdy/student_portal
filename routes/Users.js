e0e54a844f53471f44eaccfccb55804c74e4cf10
Dhanushka <dhanushka.udy@gmail.com>
2019-08-06 05:27:47 +0530

    part 3 done. completed register route and loging route.

--- routes/Users.js
+++ routes/Users.js
@@ -33,8 +33,51 @@ users.post('/register', (req, res) => {
     })
         .then(user => {
             if(!user){
-
+                bcrypt.hash(req.body.password, '10',(err, hash) => {
+                    userData.password = hash;
+                    User.create(userData)
+                        .then(user => {
+                            res.json({status: user.email + "register"})
+                        })
+                        .catch( err => {
+                            res.send("error" + err);
+                        })
+                    })
+            }else{
+                res.json({error: "user already registered"});
             }
+        })
+        .catch( err => {
+            res.send("error" + err);
+        })
+});
+
+users.post('/login', (req, res)=>{
+    User.findOne({
+        email : req.body.email
+    })
+        .then( user => {
+            if(user){
+                if(bcrypt.compareSync(req.body.password, user.password)){
+                    const payload = {
+                        _id : user._id,
+                        first_name: user.first_name,
+                        last_name: user.last_name,
+                        email: user.email
+                    };
+                    let toke = jwt.sign(payload, process.env.SECRET_KEY,{
+                        expiresIn: 1440
+                    });
+                    res.send(token)
+                }else{
+                    res.json({error: "user doesnt exist in the system"});
+                }
+            }else{
+                res.json({error : "user doesnt exist in the system"});
+            }
+        })
+        .catch( err => {
+            res.send("error" + err);
         })
 });
 
